9df6f04f6dd33d280761b64cf19b96a8cb408f92
MartinArispeRiveros <martinarisperiveros@gmail.com>
2014-09-16 09:58:28 -0400

    correccion de titulo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	# Returns the full title on a per-page basis.
   	def full_title(page_title)
-    	base_title = "INFORMATICACOMP SRL"
+    	base_title = "FERRETERIA MOLINA"
     	if page_title.empty?
       		base_title
     	else
